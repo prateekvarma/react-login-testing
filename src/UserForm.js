78308ef1fb39905fc7e4bcb2cac3822d3431352e
prateekvarma <prtk.varma@gmail.com>
2023-05-02 11:50:30 -0600

    Added states

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
+
 function UserForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, email);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Name</label>
-        <input />
+        <input value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
         <label>Email</label>
-        <input />
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <button>Add user</button>
     </form>
