2f96eb5e5153cde76184556986e95d06b9ebb9af
prateekvarma <prtk.varma@gmail.com>
2023-05-02 11:39:03 -0600

    Created components

---
+++ src/UserForm.js
@@ -0,0 +1,17 @@
+function UserForm() {
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input />
+      </div>
+      <div>
+        <label>Email</label>
+        <input />
+      </div>
+      <button>Add user</button>
+    </form>
+  );
+}
+
+export default UserForm;
