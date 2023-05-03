e35eb2e4974ee60356225a134a8280eace2ac68f
prateekvarma <prtk.varma@gmail.com>
2023-05-03 11:16:00 -0600

    Displaying users list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import UserForm from './UserForm';
+import UserList from './UserList';
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -10,6 +12,8 @@ function App() {
   return (
     <div>
       <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
     </div>
   );
 }
