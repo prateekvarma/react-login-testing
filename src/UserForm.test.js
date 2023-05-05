e34d49f30f9b16022cbef30ed50e787ef0c09dbe
prateekvarma <prtk.varma@gmail.com>
2023-05-05 13:24:21 -0600

    Temporary test - to simulate filling a form and submitting

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { user } from '@testing-library/user-event';
+import user from '@testing-library/user-event';
 import UserForm from './UserForm';
 
 test('it shows two inputs and a button', () => {
@@ -10,4 +10,25 @@ test('it shows two inputs and a button', () => {
 
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument;
+});
+
+test('it calls onUserAdd when the form is submitted', () => {
+  //not the best way to do this
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
+
+  render(<UserForm onUserAdd={callback} />);
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  const button = screen.getByRole('button');
+
+  user.click(nameInput);
+  user.keyboard('jane');
+  user.click(emailInput);
+  user.keyboard('jane@jane.com');
+  user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({name: 'jane', email: 'jane@jane.com'});
 });
