9d1f4e32a4d7ce68ad92fa5f96c31606225c9749
prateekvarma <prtk.varma@gmail.com>
2023-05-09 16:23:43 -0600

    Tested rows of data using querySelectorAll

---
+++ src/UserList.test.js
@@ -0,0 +1,17 @@
+import { render, screen, within } from '@testing-library/react';
+import UserList from './UserList';
+
+test('render one row per user', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+
+  const { container } = render(<UserList users={users} />);
+
+  const rows = container.querySelectorAll('tbody tr')
+
+  expect(rows).toHaveLength(2);
+});
+
+test('render the email and name of each user', () => {});
