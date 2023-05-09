b42213fbeb29cfb4c5791ae0e6fe89e3bc605e67
prateekvarma <prtk.varma@gmail.com>
2023-05-09 17:00:04 -0600

    Test to check email and user on UserList

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -7,11 +7,26 @@ test('render one row per user', () => {
     { name: 'sam', email: 'sam@sam.com' },
   ];
 
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
 
-  const rows = container.querySelectorAll('tbody tr')
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
   expect(rows).toHaveLength(2);
 });
 
-test('render the email and name of each user', () => {});
+test('render the email and name of each user', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+
+  render(<UserList users={users} />);
+
+  for(let user of users) {
+    const name = screen.getByRole('cell', { name: user.name })
+    const email = screen.getByRole('cell', { name: user.email })
+
+    expect(name).toBeInTheDocument()
+    expect(email).toBeInTheDocument()
+  }
+});
