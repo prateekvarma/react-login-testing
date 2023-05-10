8e357f981ec48e600b284e854e0acf2a547d9e71
prateekvarma <prtk.varma@gmail.com>
2023-05-10 14:29:54 -0600

    Refectored tests to use a helper function

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,13 +1,19 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
-test('render one row per user', () => {
+function renderComponent() {
   const users = [
     { name: 'jane', email: 'jane@jane.com' },
     { name: 'sam', email: 'sam@sam.com' },
   ];
 
   render(<UserList users={users} />);
+
+  return { users };
+}
+
+test('render one row per user', () => {
+  renderComponent();
 
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
@@ -15,18 +21,13 @@ test('render one row per user', () => {
 });
 
 test('render the email and name of each user', () => {
-  const users = [
-    { name: 'jane', email: 'jane@jane.com' },
-    { name: 'sam', email: 'sam@sam.com' },
-  ];
+  const { users } = renderComponent();
 
-  render(<UserList users={users} />);
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
 
-  for(let user of users) {
-    const name = screen.getByRole('cell', { name: user.name })
-    const email = screen.getByRole('cell', { name: user.email })
-
-    expect(name).toBeInTheDocument()
-    expect(email).toBeInTheDocument()
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
   }
 });
